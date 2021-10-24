d1dd06c53f6dbe7c8dbe3fb8bf3265065640ebe3
alterperson <vad.mk@ya.ru>
2021-10-24 20:40:10 +0300

    create: added modal window

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,14 @@ new Swiper(".swiper", {
   },
 });
 
-$(".header__contacts-burger").click(function () {
+$(".header__contacts-burger").click(() => {
   $(".header__contacts").slideToggle();
+});
+
+$(".present__order-btn").click(() => {
+  $(".page__overlay--modal").fadeIn(700).css("display", "flex");
+});
+
+$(".modal__close").click(() => {
+  $(".page__overlay--modal").fadeOut(700);
 });
